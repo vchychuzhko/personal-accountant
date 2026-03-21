f81bd8334c1bb6bcde8ece4ea8f2eb3363c05ecc
Vladyslav Chychuzhko <vlad.chichuzhko@gmail.com>
2026-03-21 22:44:04 +0100

    #1 Register form is added

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
+use App\Repository\AdminRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, AdminRepository $adminRepository): Response
     {
+        if ($adminRepository->count() === 0) {
+            $this->addFlash('info', 'Please create the first admin account.');
+
+            return $this->render('security/register.html.twig');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -22,6 +33,33 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    #[Route(path: '/register', name: 'app_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        AdminRepository $adminRepository,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+
+        if ($username && $password) {
+            $admin = new Admin();
+            $admin->setUsername($username);
+            $admin->setRoles(['ROLE_ADMIN']);
+            $admin->setPassword($passwordHasher->hashPassword($admin, $password));
+
+            $entityManager->persist($admin);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'You can now login.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig');
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
